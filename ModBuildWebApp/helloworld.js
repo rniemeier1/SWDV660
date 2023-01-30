03e8d5814c9c7527a71312a618940df9dfb6ef02
R. Niemeier <101990166+rniemeier1@users.noreply.github.com>
2023-01-29 21:02:20 -0600

    Update helloworld.js

--- ModBuildWebApp/helloworld.js
+++ ModBuildWebApp/helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Lets play video games!\n')
+   response.end('Hello Rebecca!\n')
 }).listen(3333)
 
 // Console will print the message
