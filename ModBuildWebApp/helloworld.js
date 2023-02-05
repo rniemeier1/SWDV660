8d08b185c4a82056472cc5800b66a4cdf2f494f0
Rebecca Niemeier <101990166+rniemeier1@users.noreply.github.com>
2023-02-05 16:14:12 -0600

    Update helloworld.js
    modified

--- ModBuildWebApp/helloworld.js
+++ ModBuildWebApp/helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Hello Rebecca\n')
+   response.end('Hello Rebecca using Ansible Playbook!\n')
 }).listen(3333)
 
 // Console will print the message
